fac40b75d2f947bab75b3645a357494f4f55904e
JG <754800643@qq.com>
2022-08-15 03:30:40 +0000

    Update App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "./contexts/AuthContext";
 
 import App from "./App";
 
@@ -8,6 +9,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
   </StrictMode>
 );
